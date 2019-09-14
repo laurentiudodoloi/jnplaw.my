7984716f55cf8f28209f7dfeec09dee981b18b2d
Laurentiu <dodoloilaurentiu@gmail.com>
2019-09-15 00:25:25 +0300

    Update routes.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-`Route::get('/', 'WelcomeController@index');
+Route::get('/', 'WelcomeController@index');
 
 Route::get('/about', 'AboutController@index')->name('about-us');
 
